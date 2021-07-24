c40105f9ac4cdcf20bafa672474e7355df193fb9
Nikolai Varlamov <varlamow.col@yahoo.com>
2021-07-24 20:01:03 +0300

    added UUIDs for char and svc

---
+++ main.py
@@ -0,0 +1,4 @@
+from bluepy import Peripheral, Service, Characteristic
+
+UUID_SVC_CABLE_REPLACEMENT = '0bd51666-e7cb-469b-8e4d-2742f1ba77cc'
+UUID_CHAR_CABLE_REPLACEMENT = 'e7add780-b042-4876-aae1-112855353cc1'
